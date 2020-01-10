2255b01149a2826272478bd948e76c7272f9d342
Artem <shaue188@gmail.com>
2020-01-11 01:47:13 +0500

    Add the order of moves (white alternates with black). Improve the function that determines if killing another checker is possible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from os import path
 from classes import Board, Checker
+import functions
 
 
 def load_image(name):
@@ -28,18 +29,19 @@ board = Board(left, top, cell_length, lines, rows)
 for row in range(5, 8):
     for col in range((row + 1) % 2, 8, 2):
         image = pygame.transform.scale(load_image('white_checker.png'), (cell_length, cell_length))
-        checker = Checker(left + col * cell_length, top + row * cell_length, 'white', all_sprites, image, left, top,
+        checker = Checker(col, row, 'white', all_sprites, image, left, top,
                           cell_length, lines, rows)
         board.board.append(checker)
 
 for row in range(0, 3):
     for col in range((row + 1) % 2, 8, 2):
         image = pygame.transform.scale(load_image('black_checker.png'), (cell_length, cell_length))
-        checker = Checker(left + col * cell_length, top + row * cell_length, 'black', all_sprites, image, left, top,
+        checker = Checker(col, row, 'black', all_sprites, image, left, top,
                           cell_length, lines, rows)
         board.board.append(checker)
 
 
+moving_color = 'white'
 selected_checker = None
 while running:
     for event in pygame.event.get():
@@ -49,14 +51,17 @@ while running:
             if selected_checker is None:
                 for elem in board.board:
                     if elem.rect.collidepoint(event.pos):
-                        selected_checker = elem
+                        if elem.color == moving_color:
+                            selected_checker = elem
             else:
                 x, y = board.get_cell(event.pos)
                 if (x + y) % 2:
-                    print(x, y)
-                    pos = 10 + 64 * x, 10 + 64 * y
-                    selected_checker.make_move(pos)
-                    selected_checker = None
+                    moving_checkers = [checker for checker in board.board if checker.color == moving_color]
+                    not_moving_checkers = [checker for checker in board.board if checker.color != moving_color]
+                    if functions.can_move(selected_checker, x, y, moving_checkers, not_moving_checkers):
+                        selected_checker.make_move((x, y))
+                        selected_checker = None
+                        moving_color = 'white' if moving_color == 'black' else 'black'
 
     clock.tick(FPS)
 
